845113a644f177ff1fd88ca9de2117d04f75012b
hyh <123@qq.com>
2024-09-14 21:57:56 +0800

    add cuda

--- attention.py
+++ attention.py
@@ -9,7 +9,7 @@ def NET(X):
 def averageGather(y_hat):
     return y_hat.mean(dim=0)
 
-def nonParamAttention(y_hat, x, x_hat):
+def nonParamAttention(y_hat, x, x_hat, device='cpu'):
     '''
     x is the true input
     x_hat is the train or sample input
@@ -17,6 +17,9 @@ def nonParamAttention(y_hat, x, x_hat):
     '''
     x = x.reshape((-1, 1)).expand((x.shape[0], y_hat.shape[0]))
     x_hat = x_hat.expand((x_hat.shape[0], y_hat.shape[0]))
+    x = x.to(device)
+    x_hat = x_hat.to(device)
+    y_hat = y_hat.to(device)
     # print(x, '\n', x_hat)
     up = torch.exp(-0.5*(x - x_hat)**2)
     down = torch.sum(up, dim = 1, keepdim = True)
@@ -26,7 +29,7 @@ def nonParamAttention(y_hat, x, x_hat):
     # print(result)
     return result
 
-def paramAttention(y_hat, x, x_hat, w):
+def paramAttention(y_hat, x, x_hat, w, device):
     '''
     x is the true input
     x_hat is the train or sample input
@@ -35,6 +38,9 @@ def paramAttention(y_hat, x, x_hat, w):
     '''
     x = x.reshape((-1, 1)).expand((x.shape[0], y_hat.shape[0]))
     x_hat = x_hat.expand((x_hat.shape[0], y_hat.shape[0]))
+    x = x.to(device)
+    x_hat = x_hat.to(device)
+    y_hat = y_hat.to(device)
     # print(x, '\n', x_hat)
     up = torch.exp(-0.5*((x - x_hat)*w)**2)
     down = torch.sum(up, dim = 1, keepdim = True)
@@ -47,10 +53,12 @@ def paramAttention(y_hat, x, x_hat, w):
 def meanSquareLoss(y, y_hat):
     return torch.mean((y - y_hat) ** 2)
 
-def train(y_hat, x_hat, lr = 50, epoches = 1000):
-    w = torch.ones((x_hat.shape[0], x_hat.shape[0]), dtype=torch.float32, requires_grad = True)
+def train(y_hat, x_hat, lr = 50, epoches = 1000, device = 'cpu'):
+    w = torch.ones((x_hat.shape[0], x_hat.shape[0]), dtype=torch.float32, requires_grad = True, device = device)
+    x_hat = x_hat.to(device)
+    y_hat = y_hat.to(device)
     for _ in range(epoches):
-        results = paramAttention(y_hat, x_hat, x_hat, w)
+        results = paramAttention(y_hat, x_hat, x_hat, w, device)
         loss = meanSquareLoss(y_hat, results)
         # print(loss)
         loss.sum().backward()
@@ -65,17 +73,23 @@ if __name__ == "__main__":
     n_train = 1000
     ranges = 50.0
     noise = 0.1
+    if torch.cuda.is_available():
+        device = 'cuda:0'
+        epoches = 10000
+    else:
+        device = 'cpu'
+        epoches = 1000
     x_hat = torch.sort(torch.rand(n_train, dtype=torch.float32) * ranges)[0]
     X = torch.arange(0., ranges, ranges/n_train, dtype = torch.float32)
     y_train = NET(x_hat) + noise * torch.randn(X.shape, dtype=torch.float32)
     plt.scatter(x_hat, y_train, s=5, c='red', label='Origin')
     plt.plot(X, NET(X), label='True')
     plt.plot(X, averageGather(y_train).expand(y_train.shape), label = 'Average Predict')
-    plt.plot(X, nonParamAttention(y_train, X, x_hat).expand(y_train.shape), label = 'Non-Param Predict')
+    plt.plot(X, nonParamAttention(y_train, X, x_hat).expand(y_train.shape).cpu(), label = 'Non-Param Predict')
     
-    w = train(y_train, x_hat)
+    w = train(y_train, x_hat, lr = 50, epoches = epoches, device = device)
     print(w)
     # print(paramAttention(y_train, X, x_hat, w))
-    plt.plot(X, paramAttention(y_train, X, x_hat, w).detach(), label = 'Param Predict')
+    plt.plot(X, paramAttention(y_train, X, x_hat, w, device = device).cpu().detach(), label = 'Param Predict')
     plt.legend()
     plt.show()
